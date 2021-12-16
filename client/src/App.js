d6565dcec43eb6b377738c0c2e973df4d4744090
Dung <ebolo6969@gmail.com>
2021-12-16 13:25:55 +0700

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,36 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import {DataProvider} from './GlobalState'
 import Header from './components/headers/Header'
 import MainPages from './components/mainpages/Pages'
+import {useDispatch, useSelector} from 'react-redux'
+import axios from 'axios';
+import {dispatchLogin, fetchUser, dispatchGetUser} from './redux/actions/authAction'
 function App() {
+  const dispatch = useDispatch()
+  const token = useSelector(state => state.token)
+  const auth = useSelector(state => state.auth)
+  useEffect(() => {
+    const firstLogin = localStorage.getItem('firstLogin')
+    if(firstLogin){
+      const getToken = async () => {
+        const res = await axios.post('/user/refresh_token', null)
+        dispatch({type: 'GET_TOKEN', payload: res.data.access_token})
+      }
+      getToken()
+    }
+  },[auth.isLogged, dispatch])
 
+  useEffect(() => {
+    if(token){
+      const getUser = () => {
+        dispatch(dispatchLogin())
+
+        return fetchUser(token).then(res => {
+          dispatch(dispatchGetUser(res))
+        })
+      }
+      getUser()
+    }
+  },[token, dispatch])
   return (
     <DataProvider>
       <Router>
