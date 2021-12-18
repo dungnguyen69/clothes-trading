1e4c2e20a4bf0da0b71794f2f4e41c8dde69e88b
Dung <ebolo6969@gmail.com>
2021-12-18 08:44:27 +0700

    Updated

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -39,7 +39,7 @@ const sendEmail = (to, url, txt) => {
     const mailOptions = {
         from: SENDER_EMAIL_ADDRESS,
         to: to,
-        subject: "DevAT Channel",
+        subject: "Group 04",
         html: `
             <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
             <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to Group 04.</h2>
